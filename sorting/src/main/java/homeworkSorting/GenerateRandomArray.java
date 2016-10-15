64f3700b2331bad9d5da037aba0f5abb0c86648a
Nolfgk <pitsbourg@yandex.ru>
2016-10-15 17:39:46 +0300

    adder random array generator

---
+++ sorting/src/main/java/homeworkSorting/GenerateRandomArray.java
@@ -0,0 +1,21 @@
+package homeworkSorting;
+
+import java.util.Random;
+
+/**
+ * Created by timur on 15.10.16.
+ */
+public class GenerateRandomArray {
+
+    public static int[] generateRandomArray() {
+        final Random random = new Random();
+        int arraySize = random.nextInt((10000) + 1);
+        System.out.println("arraySize = " + arraySize);
+        int numbersArray[] = new int[arraySize];
+
+        for (int i = 0; i<arraySize; i++) {
+            numbersArray[i] = random.nextInt((1000) + 1);
+        }
+        return numbersArray;
+    }
+}
--- sorting/src/main/java/homeworkSorting/MainClassSorting.java
+++ sorting/src/main/java/homeworkSorting/MainClassSorting.java
@@ -1,4 +1,5 @@
 package homeworkSorting;
+import java.util.Random;
 
 /**
  * Created by timur on 15.10.16.
@@ -7,6 +8,9 @@ public class MainClassSorting {
 
     public static void main(String[] args){
 
+        int[] arrayToSort = GenerateRandomArray.generateRandomArray();
+        for (int i = 0; i<arrayToSort.length; i++) {
+            System.out.println(i+1 + " element in arrayToSort = " + arrayToSort[i]);
+        }
     }
-
 }
