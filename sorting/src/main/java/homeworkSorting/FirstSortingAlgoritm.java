f9bf7ceecfb0fdca1e31c99e315a537af9801009
Nolfgk <pitsbourg@yandex.ru>
2016-10-15 19:51:22 +0300

    вылдаолдвао

--- sorting/src/main/java/homeworkSorting/FirstSortingAlgoritm.java
+++ sorting/src/main/java/homeworkSorting/FirstSortingAlgoritm.java
@@ -9,9 +9,6 @@ public class FirstSortingAlgoritm {
 
     public static int[] sortBubble(int[] arrayToSort) {
 
-        int[] sortedArray = new int[arrayToSort.length];
-        //create array that we will return
-
         for (int j = 1; j < arrayToSort.length; j++){
             for (int i = 0; i < (arrayToSort.length - j); i++) {
                 if (arrayToSort[i] > arrayToSort[i + 1]) {
