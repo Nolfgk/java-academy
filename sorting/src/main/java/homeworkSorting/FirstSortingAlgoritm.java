f84f78f29b661d1bf59d5b900244f277afc94503
Nolfgk <pitsbourg@yandex.ru>
2016-10-15 17:39:46 +0300

    adder random array generator

--- sorting/src/main/java/homeworkSorting/FirstSortingAlgoritm.java
+++ sorting/src/main/java/homeworkSorting/FirstSortingAlgoritm.java
@@ -1,7 +1,16 @@
 package homeworkSorting;
 
+import java.util.Random;
+
 /**
  * Created by timur on 15.10.16.
  */
 public class FirstSortingAlgoritm {
+
+    final Random random = new Random();
+
+
+
+
+
 }
--- sorting/src/main/java/homeworkSorting/MainClassSorting.java
+++ sorting/src/main/java/homeworkSorting/MainClassSorting.java
@@ -1,10 +1,5 @@
 package homeworkSorting;
-<<<<<<< HEAD
-=======
 import java.util.Random;
-import static homeworkSorting.FirstSortingAlgoritm.sortBubble;
-
->>>>>>> 4f41856... Получилось реализовать первый метод сортировки (пузырьковый).
 
 /**
  * Created by timur on 15.10.16.
@@ -13,25 +8,9 @@ public class MainClassSorting {
 
     public static void main(String[] args){
 
-<<<<<<< HEAD
-=======
         int[] arrayToSort = GenerateRandomArray.generateRandomArray();
-        for (int i = 0; i < arrayToSort.length; i++) {
+        for (int i = 0; i<arrayToSort.length; i++) {
             System.out.println(i+1 + " element in arrayToSort = " + arrayToSort[i]);
         }
-
-        int[] sortedArray = sortBubble(arrayToSort);
-
-        for (int i = 0; i < sortedArray.length; i++) {
-            System.out.println(i+1 + " element in sortedArray = " + sortedArray[i]);
-        }
-
-
-
-
-
-
->>>>>>> 4f41856... Получилось реализовать первый метод сортировки (пузырьковый).
     }
-
 }
