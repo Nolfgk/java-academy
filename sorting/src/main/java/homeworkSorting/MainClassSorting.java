fa1b7a2630f3d89ba3dc6a23e5d6b2c64b1b324e
Nolfgk <pitsbourg@yandex.ru>
2016-10-15 19:32:16 +0300

    добавил второй метод сортировки в главный класс

--- sorting/src/main/java/homeworkSorting/MainClassSorting.java
+++ sorting/src/main/java/homeworkSorting/MainClassSorting.java
@@ -1,6 +1,7 @@
 package homeworkSorting;
 import java.util.Random;
 import static homeworkSorting.FirstSortingAlgoritm.sortBubble;
+import static homeworkSorting.SecondSorting.sortSilly;
 
 
 /**
@@ -19,6 +20,18 @@ public class MainClassSorting {
 
         for (int i = 0; i < sortedArray.length; i++) {
             System.out.println(i+1 + " element in sortedArray = " + sortedArray[i]);
+        }
+
+
+        int[] arrayToSort2 = GenerateRandomArray.generateRandomArray();
+        for (int i = 0; i < arrayToSort2.length; i++) {
+            System.out.println(i+1 + " element in arrayToSort2 = " + arrayToSort2[i]);
+        }
+
+        int[] sortedArray2= sortSilly(arrayToSort2);
+
+        for (int i = 0; i < sortedArray2.length; i++) {
+            System.out.println(i+1 + " element in sortedArray2 = " + sortedArray2[i]);
         }
 
 
